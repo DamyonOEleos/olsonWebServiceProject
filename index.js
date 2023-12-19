d9640345e647cf13bc0c05abf26e04b7d9ec5ae0
Damyon <80194972+DamyonO@users.noreply.github.com>
2023-12-19 13:13:19 -0500

    POST created for authenticate

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-const app = require('express')();
+const express = require("express");
+const app = express();
 const PORT = 8080;
+
+app.use(express.json())
 
 app.listen(
     PORT,
     () => console.log(`API ready at http://localhost:${PORT}`)
 )
+
+/*
+    Updates needed for the following call : 
+        * Connect to a DB (MongoDB or other)
+        * Only res.send if the username exists in the db, as well as if the username matches
+          the password within the DB
+*/
+app.post('/authenticate', (req, res) => {
+    const { username } = req.body
+    const { password } = req.body
+    const { is_team_driver_login } = req.body
+
+    if(!username | !password) {
+        res.status(401).send({message: 'Unauthorized due to invalid username or password.'})
+    }
+
+    res.send({
+        user: `User with the username ${username} found`,
+    })
+})
+
